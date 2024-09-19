0975e5c58512743a739e24a910710ee7da336989
Vladyslav Terebylo <terebylov@ssemi.cz>
2024-09-19 16:44:46 +0200

    Initialization of project

---
+++ src/main/java/com/spheret/SpheretApplication.java
@@ -0,0 +1,13 @@
+package com.spheret;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpheretApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpheretApplication.class, args);
+    }
+
+}
